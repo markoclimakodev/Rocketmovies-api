6f2d3e5fd08e23fd8ab13b16894b5848d37bab93
markoclimako <mkclimako@gmail.com>
2022-11-28 19:06:26 -0300

    feat(crontrollers| users): create user  controllers

---
+++ src/controllers/users/UserController.ts
@@ -0,0 +1,49 @@
+import { Request, Response } from 'express';
+
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+export class UserController {
+  async create(request: Request, response: Response) {
+    try {
+      const { name, email, password, avatar } = request.body;
+      console.log(name, email, password, avatar);
+
+      const user = await prisma.users.create({
+        data: {
+          name,
+          email,
+          password,
+          avatar,
+        },
+      });
+      return response.status(201).json({ user });
+    } catch (error) {
+      console.error(error);
+      response.sendStatus(500);
+    }
+  }
+
+  async update(request: Request, response: Response) {
+    try {
+      const { userId } = request.params;
+      const { name, email, password, avatar } = request.body;
+
+      const user = await prisma.users.update({
+        where: {
+          id: userId,
+        },
+        data: {
+          name,
+          email,
+          password,
+          avatar,
+        },
+      });
+      return response.status(201).json({ user });
+    } catch (error) {
+      console.error(error);
+      response.sendStatus(500);
+    }
+  }
+}
